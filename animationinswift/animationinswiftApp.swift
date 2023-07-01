c947a6b6b66aa6336badf19e335a0ebd1bcdc5e5
Qudus Qazeem <qudusqazeem@gmail.com>
2023-07-01 17:12:16 +0100

    Initial Commit

---
+++ animationinswift/animationinswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationinswiftApp.swift
+//  animationinswift
+//
+//  Created by Qazeem Abiodun on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct animationinswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
